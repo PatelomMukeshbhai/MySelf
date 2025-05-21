77d23ef9aeafc3ed5506cc1b32eb2c03e0a8cad0
om patel <om@patelom.onmicrosoft.com>
2025-05-21 23:24:39 +0530

    Add project files.

---
+++ Controllers/ShowUserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySelf.Controllers
+{
+    public class ShowUserController : Controller
+    {
+        // GET: ShowUser
+        public ActionResult Price()
+        {
+            return View();
+        }
+    }
+}
